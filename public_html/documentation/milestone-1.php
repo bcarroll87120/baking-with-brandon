4694267fd5b4dab5c7d33f99998454d244205ed9
Brandon Carroll <bcarroll@live.com>
2019-01-23 21:36:51 -0700

    Created milestone-1 for a plant-based food blog.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Baking with Brandon</title>
+	</head>
+	<body>
+		<h1>Baking with Brandon</h1>
+		<p>My personal website will be title Baking with Brandon. It is a strictly whole food plant-based food blog
+			for anyone interested in looking for plant-based recipes. The goal is to help anyone interested in adopting
+			or maintaining a plant-based diet.</p>
+		<h2>Persona</h2>
+		<p>Bill:</p>
+		<ul>
+			<li>Age: early twenties</li>
+			<li>Job: food joint near local campus. Full-time college student</li>
+			<li>Behavior: outgoing; interested in social causes; cares about animals, the environment, and personal health</li>
+			<li>Android phone and Acer laptop</li>
+			<li>Goals regarding use of website: to learn how to make cheap and healthy plant-based meals</li>
+			<li>Frustrations: unable to find food or recipes that are cheap, healthy, and/or plant-based.</li>
+		</ul>
+	</body>
+</html>
