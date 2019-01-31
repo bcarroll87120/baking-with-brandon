25f5837c692fa57027464b48410b436e3c3db2f4
Brandon Carroll <bcarroll@live.com>
2019-01-31 10:24:58 -0700

    Created milestone-1 for a plant-based food blog.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,18 +14,14 @@
 		<img src="millenial2.jpg" alt="Bill">
 		<ul>
 			<li>Age: 22.</li>
-			<li>Job: food joint near local campus. Bill is also a full-time college student</li>
+			<li>Job: local food joint near his college campus. Bill is also a full-time college student</li>
 			<li>Behavior: Generally outgoing. Has a moderate circle of friends and acquaintances that generally
 				care about social issues. Bill has a particular care for animals and has a cat, the environment, and personal health
 				Bill has begun watching and reading topics and articles pertaining to the relationship between
 			</li>
 			<li>Samsung Galaxy Note9 phone and Acer Aspire E15 laptop running Windows 10</li>
 			<li>Goals regarding use of website: to learn how to make cheap and healthy plant-based meals</li>
-			<li>Frustrations: unable to find food or recipes that are cheap, healthy, and/or plant-based.
-			Bill has started frequenting "vegan" food blogs and finds the recipes often difficult or containing
-			way too many ingredients. Many of these recipes often call for unhealthy sugars, oils, and refined ingredients
-				that don't promote the "whole food plant-based" diet that he wants to follow. He also gets annoyed by the
-				multiple paragraphs that he has to scroll through just to get to each recipe.</li>
+			<li>Frustrations: unable to find food or recipes that are cheap, healthy, and/or plant-based. Bill has started frequenting "vegan" food blogs and finds the recipes often difficult or containing way too many ingredients. Many of these recipes often call for unhealthy sugars, oils, and refined ingredients that don't promote the "whole food plant-based" diet that he wants to follow. He also gets annoyed by the multiple paragraphs that he has to scroll through just to get to each recipe.</li>
 		</ul>
 	</body>
 </html>
