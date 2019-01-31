ed1a139d595d197e70529967c6cb6897c565f594
Brandon Carroll <bcarroll@live.com>
2019-01-31 10:04:46 -0700

    Created milestone-1 for a plant-based food blog.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,12 +11,12 @@
 			or maintaining a plant-based diet.</p>
 		<h2>Persona</h2>
 		<p>Bill:</p>
-		<img src=".millenial2.jpg">
+		<img src="millenial2.jpg" alt="Bill">
 		<ul>
-			<li>Age: early twenties</li>
-			<li>Job: food joint near local campus. Full-time college student</li>
+			<li>Age: 22.</li>
+			<li>Job: food joint near local campus. Bill is also a full-time college student</li>
 			<li>Behavior: outgoing; interested in social causes; cares about animals, the environment, and personal health</li>
-			<li>Android phone and Acer laptop</li>
+			<li>Samsung Galaxy Note9 phone and Acer Aspire E15 laptop</li>
 			<li>Goals regarding use of website: to learn how to make cheap and healthy plant-based meals</li>
 			<li>Frustrations: unable to find food or recipes that are cheap, healthy, and/or plant-based.</li>
 		</ul>
