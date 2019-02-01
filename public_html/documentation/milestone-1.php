2a81d4ba0cba13c7b10bc32c16a295a71bdd27c6
Brandon Carroll <bcarroll@live.com>
2019-01-31 20:06:17 -0700

    Created milestone-1 for a plant-based food blog.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,19 +6,19 @@
 	</head>
 	<body>
 		<h1>Baking with Brandon</h1>
-		<p>My personal website will be title Baking with Brandon. It is a strictly whole food plant-based food blog
-			for anyone interested in looking for plant-based recipes. The goal is to help anyone interested in adopting
-			or maintaining a plant-based diet.</p>
+		<p><strong>Purpose:</strong> The purpose of my PWP would be to share whole food plant-based recipes for those interested in either adopting or maintaining such a diet.</p>
+		<p><strong>Audience:</strong> The audience would simply be anyone interested in cooking more plant-based recipes at home.</p>
+		<p><strong>Goal:</strong> To provide recipes, cooking, tips, and food info to anyone interested in plant-based eating.  </p>
 		<h2>Persona</h2>
-		<p>Bill:</p>
+		<p>Bill Riker:</p>
 		<img src="millenial2.jpg" alt="Bill">
 		<ul>
-			<li>Age: 22</li>
-			<li>Job: local food joint near his college campus. Bill is also a full-time college student</li>
-			<li>Behavior: Generally outgoing. Has a moderate circle of friends and acquaintances that generally care about social issues. Bill has a particular care for animals and has a cat, the environment, and personal health Bill has begun watching and reading topics and articles pertaining to the relationship between</li>
-			<li>Samsung Galaxy Note9 phone and Acer Aspire E15 laptop running Windows 10</li>
-			<li>Goals regarding use of website: to learn how to make cheap and healthy plant-based meals</li>
-			<li>Frustrations: unable to find food or recipes that are cheap, healthy, and/or plant-based. Bill has started frequenting "vegan" food blogs and finds the recipes often difficult or containing way too many ingredients. Many of these recipes often call for unhealthy sugars, oils, and refined ingredients that don't promote the "whole food plant-based" diet that he wants to follow. He also gets annoyed by the multiple paragraphs that he has to scroll through on many food blogs just to get to each recipe.</li>
+			<li><strong>Age:</strong> 22</li>
+			<li><strong>Job:</strong>s local food joint near his college campus. Bill is also a full-time college student</li>
+			<li><strong>Technology: </strong>Samsung Galaxy Note9 phone and Acer Aspire E15 laptop running Windows 10</li>
+			<li><strong>Behavior:</strong> Bill is overall outgoing. Has a moderate circle of friends and acquaintances that generally care about social issues. Bill has a particular fondness for animals and has a cat. He has gained a specific interest and concern for the environment, personal health, practices in food agriculture and factory farming, and how all of these topics relate to one another. Bill has begun watching and reading topics and articles about what’s considered a “whole food plant-based diet” that strictly relies on whole, unrefined plant foods. Bill has decided that he wants to adopt this lifestyle choice by cooking like-minded recipes.</li>
+			<li><strong>Goal: </strong>Bill’s goal is to learn how to become proficient at cooking plant-based meals regularly that are cheap and easy to understand. He wants to fundamentally change his diet and eating habits one recipe at a time until he has mostly adopted a plant-based diet.</li>
+			<li><strong>Frustrations: </strong>Bill’s frustrations include the inability to find pre-made meals or recipes that are cheap, healthy, and/or completely plant-based. Bill has started frequenting "vegan" food blogs and finds the recipes often difficult or containing too many ingredients. Many of these often call for ingredients that don't promote this lifestyle change that he wants to adopt. Many vegan restaurants or meals he buys are expensive, high in calories, and generally unhealthy He also gets annoyed by the multiple paragraphs that he has to scroll through on many food blogs just to get to each recipe.</li>
 		</ul>
 	</body>
 </html>
